2cb0760c6a98f372cfd4be99850a7b6f080025d5
javex <florian.ruechel@inexplicity.de>
2024-03-17 20:05:52 +1030

    Refactor some code into product module

--- src/stores/coles/product.rs
+++ src/stores/coles/product.rs
@@ -1,27 +1,16 @@
 use crate::errors::{Error, Result};
-use crate::product::{PriceHistory, Product, Unit};
+use crate::product::{parse_str_unit, Product, Unit};
 use std::fmt::Display;
 use std::result::Result as StdResult;
 
 use itertools::{Either, Itertools};
-use lazy_static::lazy_static;
-use regex::Regex;
 use serde::Deserialize;
 use std::io::Read;
 use tar::Archive;
-use time::OffsetDateTime;
 
 use super::category::SearchResults;
 
 const IGNORED_RESULT_TYPES: [&str; 2] = ["SINGLE_TILE", "CONTENT_ASSOCIATION"];
-
-lazy_static! {
-    static ref UNIT_REGEX: Regex = Regex::new(r#"(?P<quantity>[0-9]+) ?(?P<unit>[a-z]+)"#).unwrap();
-    static ref EACH_WORDS: Vec<&'static str> = vec![
-        "ea", "each", "pk", "pack", "bunch", "sheets", "sachets", "capsules", "ss", "set", "pair",
-        "pairs", "piece", "tablets", "rolls",
-    ];
-}
 
 #[derive(Deserialize, Debug)]
 struct PricingUnit {
@@ -93,62 +82,6 @@ fn get_quantity_and_unit(item: &SearchResult) -> Result<(f64, Unit)> {
     Ok((parsed_quantity, unit))
 }
 
-fn parse_str_unit(size: &str) -> Result<(f64, Unit)> {
-    let size = size.to_lowercase();
-    let captures = UNIT_REGEX
-        .captures(&size)
-        .ok_or(Error::ProductConversion(format!(
-            "regex didn't match for {}",
-            size
-        )))?;
-
-    let quantity: f64 = captures
-        .name("quantity")
-        .ok_or(Error::ProductConversion(format!(
-            "missing field quantity in {}",
-            size
-        )))?
-        .as_str()
-        .parse()
-        .map_err(|e| Error::ProductConversion(format!("can't parse quantity as f64: {}", e)))?;
-
-    let unit = captures
-        .name("unit")
-        .ok_or(Error::ProductConversion(format!(
-            "missing field unit for {}",
-            size
-        )))?
-        .as_str();
-    let (factor, unit) = normalise_unit(unit)?;
-    let quantity = quantity * factor;
-
-    Ok((quantity, unit))
-}
-
-fn normalise_unit(unit: &str) -> Result<(f64, Unit)> {
-    let (factor, unit) = match unit {
-        // Grams
-        "g" => (1.0, Unit::Grams),
-        "kg" => (1000.0, Unit::Grams),
-        "mg" => (0.001, Unit::Grams),
-
-        // Millilitre
-        "ml" => (1.0, Unit::Millilitre),
-        "l" => (1000.0, Unit::Millilitre),
-
-        // Centimetre
-        "cm" => (1.0, Unit::Centimetre),
-        "m" | "metre" => (100.0, Unit::Centimetre),
-
-        // Each
-        "dozen" => (12.0, Unit::Each),
-        x if EACH_WORDS.contains(&x) => (1.0, Unit::Each),
-
-        _ => return Err(Error::ProductConversion(format!("unknown unit: {}", unit))),
-    };
-    Ok((factor, unit))
-}
-
 impl TryFrom<SearchResult> for Product {
     type Error = Error;
     fn try_from(item: SearchResult) -> StdResult<Self, Self::Error> {
@@ -160,22 +93,16 @@ impl TryFrom<SearchResult> for Product {
             name = format!("{} {}", item.brand, name);
         }
 
-        let price_history = vec![PriceHistory {
-            date: OffsetDateTime::now_utc().date(),
-            price: pricing.now,
-        }];
-
         let (quantity, unit) = get_quantity_and_unit(&item)?;
-
-        let product = Product {
-            id: item.id,
+        let product = Product::new(
+            item.id,
             name,
-            description: item.description,
-            price_history,
-            is_weighted: pricing.unit.is_weighted.unwrap_or(false),
+            item.description,
+            pricing.now,
+            pricing.unit.is_weighted,
             unit,
             quantity,
-        };
+        );
         Ok(product)
     }
 }
