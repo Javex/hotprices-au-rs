cc60e74fa88b11d9efa5b6442cb4e29705590fdc
javex <florian.ruechel@inexplicity.de>
2024-03-19 22:07:28 +1030

    Add deduplication function

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,6 +1,12 @@
-use std::{fs::File, io::BufReader, path::PathBuf};
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::BufReader,
+    path::PathBuf,
+};
 
 use flate2::{read::GzDecoder, write::GzEncoder, Compression};
+use log::info;
 use tar::Archive;
 use time::Date;
 
@@ -44,6 +50,7 @@ pub fn do_analysis(
         let file = Archive::new(file);
         load_from_archive(file)?
     };
+    let products = deduplicate_products(products);
     save_result(products, output_dir)?;
     Ok(())
 }
@@ -54,4 +61,36 @@ fn save_result(products: Vec<Product>, output_dir: PathBuf) -> Result<()> {
     let file = GzEncoder::new(file, Compression::default());
     serde_json::to_writer(file, &products)?;
     Ok(())
+}
+
+fn deduplicate_products(products: Vec<Product>) -> Vec<Product> {
+    let mut lookup = HashSet::new();
+    let mut dedup_products = Vec::new();
+    let mut duplicates = HashMap::new();
+    for product in products {
+        let product_key = (product.store, product.id);
+        if lookup.contains(&product_key) {
+            *duplicates.entry(product.store).or_insert(0) += 1;
+        } else {
+            lookup.insert(product_key);
+            dedup_products.push(product);
+        }
+    }
+
+    if !duplicates.is_empty() {
+        info!("Deduplicated products: {:?}", duplicates);
+    }
+    dedup_products
+}
+
+#[cfg(test)]
+mod test {
+    use super::deduplicate_products;
+    use crate::product::Product;
+
+    #[test]
+    fn test_deduplicate() {
+        let products = vec![Product::default(), Product::default()];
+        assert_eq!(deduplicate_products(products).len(), 1);
+    }
 }
