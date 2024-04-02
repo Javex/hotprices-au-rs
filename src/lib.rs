a7ffe0da924408ee125de41e33979b7404beab12
javex <florian.ruechel@inexplicity.de>
2024-04-02 22:41:23 +1030

    Add a small amount of docs just for fun

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! hotprices.org Crate
+//!
+//! A Rust implementation of the hotprices.org scraper for Australian grocery stores. The library
+//! provides all the tools to both scrape prices from Woolworths & Coles as well as normalise them
+//! into a canonical format. Inspired by [heisse-preise.io](https://heisse-preise.io/).
+//!
+//! The library is unlikely to be all the useful to you. Its documentation exists mostly as an
+//! exercise for me, but you're welcome to experiment with it.
 pub mod analysis;
 mod cache;
 mod conversion;
--- src/sync.rs
+++ src/sync.rs
@@ -5,6 +5,28 @@ use std::fs::create_dir_all;
 use std::path::PathBuf;
 use time::OffsetDateTime;
 
+/// Scrapes stores and saves results to a local folder. Individual results will be cached until all
+/// pages of products have been scraped. Calling this function again after failure will fetch any
+/// cached results straight from the cache and only fetch missing items from stores, as long as it
+/// is the same day (in UTC timestamp).
+///
+/// # Examples
+///
+/// Simply print the path where the resulting file would be saved:
+///
+/// ```
+/// let output_dir = tempfile::tempdir().unwrap();
+/// let cache_path = tempfile::tempdir().unwrap();
+/// hotprices_au_rs::sync::do_sync(
+///   hotprices_au_rs::stores::Store::Woolies,
+///   true,  // quick
+///   true,  // print_save_path
+///   false,  // skip_existing
+///   output_dir.path().to_path_buf(),
+///   cache_path.path().to_path_buf(),
+/// ).unwrap();
+/// ```
+///
 pub fn do_sync(
     store: Store,
     quick: bool,
