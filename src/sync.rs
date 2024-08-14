ca6858e3fd907ccb6c20bfd7d2244d56afdd0ff8
javex <florian.ruechel@inexplicity.de>
2024-08-14 22:24:26 +0930

    Print correct save path relative to output dir

--- src/sync.rs
+++ src/sync.rs
@@ -2,7 +2,7 @@ use crate::cache::FsCache;
 use crate::storage::{get_snapshot_path, remove, save_fetch_data};
 use crate::stores::{coles, woolies, Store};
 use std::fs::create_dir_all;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use time::OffsetDateTime;
 
 /// Scrapes stores and saves results to a local folder. Individual results will be cached until all
@@ -38,7 +38,7 @@ pub fn do_sync(
     let day = OffsetDateTime::now_utc().date();
     let snapshot_path = get_snapshot_path(&output_dir, store, day);
     if print_save_path {
-        print!("{}", snapshot_path.to_string_lossy());
+        print!("{}", get_save_path(&snapshot_path, &output_dir));
         return Ok(());
     }
 
@@ -60,4 +60,28 @@ pub fn do_sync(
     save_fetch_data(fetch_data, &snapshot_path)?;
     remove(&cache_path)?;
     Ok(())
+}
+
+fn get_save_path<'a>(snapshot_path: &'a Path, base_dir: &Path) -> std::borrow::Cow<'a, str> {
+    snapshot_path
+        .strip_prefix(base_dir)
+        .expect("should be relative base")
+        .to_string_lossy()
+}
+
+#[cfg(test)]
+mod test {
+    use time::macros::datetime;
+
+    use super::*;
+
+    #[test]
+    fn save_path() {
+        let output_dir = PathBuf::from("test");
+        let store = Store::Coles;
+        let day = datetime!(2020-12-01 0:00 UTC).date();
+        let snapshot_path = get_snapshot_path(&output_dir, store, day);
+        let res = get_save_path(&snapshot_path, &output_dir);
+        assert_eq!(res, "coles/2020-12-01.json.gz");
+    }
 }
